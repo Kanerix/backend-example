1c691def180e8eab85535dcae1b185133b427a1a
Kasper Jønsson <daekjo@egmont.com>
2024-08-22 09:54:55 +0200

    Gave proper names to resources and data blocks

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
-data "azurerm_subscription" "current" {
+data "azurerm_subscription" "primary" {
   subscription_id = "5509a305-b67f-4d6c-804e-b38fe72dc105"
 }
 
-data "github_repository" "current" {
+data "github_repository" "primary" {
   full_name = "lerpz-com/${local.repository_name}"
 }
 
-resource "azurerm_resource_group" "main" {
-  name     = "${data.github_repository.current.name}-${var.deploy_env}"
+resource "azurerm_resource_group" "app" {
+  name     = "${data.github_repository.primary.name}-${var.deploy_env}"
   location = local.location
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,5 @@
 variable "deploy_env" {
   description = "Deployment environment"
   type        = string
+  default     = "stag"
 }
