0f1f5e492e9662444f61f8a9238cc445c8a04da4
Kasper Jønsson <daekjo@egmont.com>
2024-08-22 10:34:48 +0200

    Gave githook right name

--- .githooks/pre-push.sh
+++
@@ -1,7 +0,0 @@
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-
-if [[ $BRANCH == "main" | $BRANCH == "master" ]]; then
-    cargo clippy -- -D warnings
-elif [[ $BRANCH == "staging" ]]; then
-    cargo clippy --
-fi
