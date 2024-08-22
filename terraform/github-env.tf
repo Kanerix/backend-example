bc5893a20b3cf2d8cc6cbabde0cbc520f36a11a5
Kasper Jønsson <daekjo@egmont.com>
2024-08-22 10:53:29 +0200

    Added required vars

---
+++ terraform/github-env.tf
@@ -0,0 +1,32 @@
+resource "random_string" "pwd_secret" {
+  length  = 32
+  special = true
+}
+
+resource "github_actions_environment_variable" "env" {
+  repository    = data.github_repository.primary.name
+  environment   = var.deploy_env
+  variable_name = "ENV"
+  value         = var.deploy_env
+}
+
+resource "github_actions_environment_secret" "database_url" {
+  repository      = data.github_repository.primary.name
+  environment     = var.deploy_env
+  secret_name     = "DATABASE_URL"
+  plaintext_value = "Filled in later"
+}
+
+resource "github_actions_environment_variable" "api_origin" {
+  repository    = data.github_repository.primary.name
+  environment   = var.deploy_env
+  variable_name = "API_ORIGIN"
+  value         = "https://api.lerpz.com"
+}
+
+resource "github_actions_environment_secret" "pwd_secret" {
+  repository      = data.github_repository.primary.name
+  environment     = var.deploy_env
+  secret_name     = "PWD_SECRET"
+  plaintext_value = random_string.pwd_secret.result
+}
