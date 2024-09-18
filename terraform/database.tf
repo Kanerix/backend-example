3058701168d611e72ee6cb8cdd16230cd9ca398e
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-09-18 16:17:28 +0200

    Created data reference to database

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,33 +1,15 @@
-resource "azurerm_postgresql_flexible_server" "app" {
-  name                = "${local.repository_name}-pgs"
-  resource_group_name = azurerm_resource_group.app.name
-  location            = azurerm_resource_group.app.location
+data "azurerm_postgresql_flexible_server" "primary" {
+  name                = "lerpz-infrastructure-pgs"
+  resource_group_name = "lerpz-infrastructure-database"
+}
 
-  administrator_login    = var.database_username
-  administrator_password = var.database_password
-  sku_name               = "B_Standard_B1ms"
-
-  storage_mb            = 32768
-  version               = "12"
-  auto_grow_enabled     = false
-  backup_retention_days = 7
-
-  delegated_subnet_id           = azurerm_subnet.app.id
-  private_dns_zone_id           = azurerm_private_dns_zone.app.id
-  public_network_access_enabled = false
-
-  identity {
-    type = "UserAssigned"
-
-    identity_ids = [
-      azurerm_user_assigned_identity.app.id
-    ]
-  }
+resource "azurerm_postgresql_flexible_server_database" "app" {
+  name      = "${local.repository_name}-${var.deploy_env}"
+  server_id = data.azurerm_postgresql_flexible_server.primary.id
+  collation = "en_US.utf8"
+  charset   = "utf8"
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes = [
-      zone
-    ]
   }
 }
