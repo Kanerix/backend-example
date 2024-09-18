bbe02ceccaf564adec40238fb59ac13e4f3c0c5c
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-09-18 16:17:59 +0200

    Created log workplace for application

--- terraform/logging.tf
+++ terraform/logging.tf
@@ -3,5 +3,5 @@ resource "azurerm_log_analytics_workspace" "app" {
   location            = azurerm_resource_group.app.location
   resource_group_name = azurerm_resource_group.app.name
   sku                 = "PerGB2018"
-  retention_in_days   = 7
+  retention_in_days   = 31
 }
