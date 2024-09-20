12d79d42addf76055b5e7f5622fd35db104acc6c
Kasper Jønsson <dkkasjoe@hotmail.com>
2024-09-20 15:40:25 +0200

    Fixed wrong subnet mask

--- terraform/network.tf
+++ terraform/network.tf
@@ -11,5 +11,5 @@ resource "azurerm_subnet" "app" {
   name                 = "${local.repository_name}-${var.deploy_env}"
   resource_group_name  = data.azurerm_resource_group.infrastructure_network.name
   virtual_network_name = data.azurerm_virtual_network.primary.name
-  address_prefixes     = [var.deploy_env == "prod" ? "10.0.2.0/23" : "10.0.3.0/23"]
+  address_prefixes     = [var.deploy_env == "prod" ? "10.0.2.0/23" : "10.0.4.0/23"]
 }
