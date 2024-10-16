ed6ce0d616d65da2fc4253ca0c8825b775ff7d50
Kasper <57803719+Kanerix@users.noreply.github.com>
2024-10-16 19:47:47 +0200

    Created refresh token migration

--- migrations/20240607160804_user.sql
+++ migrations/20240607160804_user.sql
@@ -4,21 +4,21 @@ CREATE TYPE role AS ENUM ('user', 'moderator', 'admin');
 
 CREATE TABLE
     users (
-        id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+        id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
         username VARCHAR(32) NOT NULL UNIQUE,
         email VARCHAR(255) NOT NULL UNIQUE,
         role role NOT NULL DEFAULT 'user',
-        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW ()
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
     );
 
 CREATE TABLE
     passwords (
-        id uuid PRIMARY KEY DEFAULT uuid_generate_v4 (),
+        id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
         user_id uuid NOT NULL,
         hash VARCHAR(127) NOT NULL,
         salt VARCHAR(36) DEFAULT NULL,
-        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW (),
-        CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+        updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+        CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     )
