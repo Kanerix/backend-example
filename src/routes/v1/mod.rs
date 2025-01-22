f916c83a4bf00e4e80770c87f7db7075cd633465
Kasper Jønsson <dkkasjoe@hotmail.com>
2025-01-22 09:11:36 +0100

    Removed old boilerplate

--- src/routes/v1/mod.rs
+++ src/routes/v1/mod.rs
@@ -16,9 +16,6 @@ use aide::swagger::Swagger;
 use axum::Extension;
 use axum::Json;
 
-use auth as auth_service;
-use posts as posts_service;
-
 use sqlx::PgPool;
 
 pub fn routes(state: PgPool) -> ApiRouter {
@@ -26,16 +23,14 @@ pub fn routes(state: PgPool) -> ApiRouter {
 		.route("/swagger", Swagger::new("/api/v1/api.json").axum_route())
 		.route("/api.json", get(serve_api))
 		.api_route("/health", get_with(health::handler, health::docs))
-		.nest_api_service("/auth", auth_service::routes(state.clone()))
-		.nest_api_service("/posts", posts_service::routes(state.clone()));
+		.nest_api_service("/auth", auth::routes(state.clone()))
+		.nest_api_service("/posts", posts::routes(state.clone()));
 
 	#[cfg(debug_assertions)]
 	let router = router.api_route("/failure", get_with(failure::handler, failure::docs));
 
 	router
 }
-
-pub struct ApiDoc;
 
 async fn serve_api(Extension(api): Extension<Arc<OpenApi>>) -> impl IntoApiResponse {
 	Json(api)
