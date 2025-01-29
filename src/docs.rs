b21e37bc5d0bc34a86f498d5bee1df0a51cdcc1d
Kasper Jønsson <dkkasjoe@hotmail.com>
2025-01-29 16:10:33 +0100

    More API docs

--- src/docs.rs
+++ src/docs.rs
@@ -1,11 +1,25 @@
 use aide::{
-	openapi::Tag,
+	openapi::{Contact, License, Tag},
 	transform::TransformOpenApi,
 };
 
 pub fn api_docs(api: TransformOpenApi) -> TransformOpenApi {
 	api.title("Lerpz API")
 		.summary("An example REST application")
+		.description("This API handles different operations for a blog application.")
+        .tos("https://lerpz.com/tos")
+        .contact(Contact {
+            name: Some("Lerpz Support".into()),
+            url: Some("https://support.lerpz.com".into()),
+            email: Some("support@lerpz.com".into()),
+            ..Default::default()
+        })
+        .license(License {
+            name: "Apache 2.0".into(),
+            identifier: Some("Apache-2.0".into()),
+            url: Some("https://www.apache.org/licenses/LICENSE-2.0.txt".into()),
+            ..Default::default()
+        })
 		.tag(Tag {
 			name: "auth".into(),
 			description: Some("Authentication".into()),
