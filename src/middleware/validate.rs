32f8bcce211f585eb51f3ea8786f2f538ebb5931
Kasper <dkkasjoe@hotmail.com>
2025-02-09 21:27:49 +0100

    Validator for Form

--- src/middleware/validate.rs
+++ src/middleware/validate.rs
@@ -1,5 +1,5 @@
 use aide::OperationInput;
-use axum::{extract::{FromRequest, Request}, http::StatusCode, Json};
+use axum::{extract::{FromRequest, Request}, http::StatusCode, Form, Json};
 use serde::{de::DeserializeOwned, Serialize};
 use validator::{Validate, ValidationErrors};
 
@@ -22,6 +22,34 @@ struct ValidationErrorItem {
 	message: String,
 }
 
+impl<S, T> FromRequest<S> for Validated<Form<T>>
+where
+	S: Send + Sync,
+	T: DeserializeOwned + Validate,
+{
+	type Rejection = HandlerError;
+
+	async fn from_request(req: Request, s: &S) -> Result<Self, Self::Rejection> {
+		let form = Form::<T>::from_request(req, s)
+			.await
+			.map_err(|_| HandlerError::new(
+				StatusCode::BAD_REQUEST,
+				"Invalid request",
+				"Couldn't parse your request."
+			))?;
+
+		form.0.validate().map_err(|_| HandlerError::new(
+			StatusCode::BAD_REQUEST,
+			"Validation failed",
+			"Was unable to validate you request."
+		))?;
+
+		Ok(Validated(form))
+	}
+}
+
+impl<T> OperationInput for Validated<Form<T>> {}
+
 impl<S, T> FromRequest<S> for Validated<Json<T>>
 where
 	S: Send + Sync,
@@ -30,7 +58,7 @@ where
 	type Rejection = HandlerError;
 
 	async fn from_request(req: Request, s: &S) -> Result<Self, Self::Rejection> {
-		let json = axum::extract::Json::<T>::from_request(req, s)
+		let json = Json::<T>::from_request(req, s)
 			.await
 			.map_err(|_| HandlerError::new(
 				StatusCode::BAD_REQUEST,
